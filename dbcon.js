ec8dc25c0da7fd68f2e1ca4a8cbb6c543f4ee92c
Tom Fattah <thomas.fattah@gmail.com>
2020-05-29 15:55:54 -0400

    Separated orders into orderRouter.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : '',
-  password        : '',
-  database        : '',
+  connectionLimit: 10,
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_fattaht',
+  password: 'zashox-meXcav-0winva',
+  database: 'cs340_fattaht',
 });
 
 module.exports.pool = pool;
---
+++ routes/orderRouter.js
@@ -0,0 +1,43 @@
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  mysql = require('../dbcon.js');
+
+const orderRouter = express.Router();
+orderRouter.use(bodyParser.urlencoded({ extended: false }));
+orderRouter.use(bodyParser.json());
+
+orderRouter
+  .route('/')
+  .get((req, res, next) => {
+    let sql =
+      'SELECT `orderID`, `customerID`, `orderDate`, `galacticPay`, `orderBeamed` FROM `Orders`';
+    let query = mysql.pool.query(sql, (err, results) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.render('orders', { results: results });
+    });
+  })
+  .post((req, res, next) => {
+    if (req.body['addOrder']) {
+      mysql.pool.query(
+        'INSERT INTO `Orders` (`customerID`, `orderBeamed`, `orderDate`, `galacticPay`) VALUES (?,?,?,?)',
+        [
+          req.body.customerID,
+          req.body.orderBeamed,
+          req.body.orderDate,
+          req.body.galaticPay,
+        ],
+        (err, results) => {
+          if (err) {
+            next(err);
+            return;
+          }
+          res.render('orders', { results: results });
+        }
+      );
+    }
+  });
+
+module.exports = orderRouter;
